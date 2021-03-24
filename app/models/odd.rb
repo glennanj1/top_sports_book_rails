9ef6d1e875add9e6f5b43153d4a8b782bd6c494d
github username <github email address>
2021-03-24 13:30:58 -0400

    added relationship

--- app/models/odd.rb
+++ app/models/odd.rb
@@ -1,6 +1,7 @@
 class Odd < ApplicationRecord
     belongs_to :sport 
     has_many :bets 
+    has_many :users, through: :bets
 
     validates :commence_time, uniqueness: true
     accepts_nested_attributes_for :bets
