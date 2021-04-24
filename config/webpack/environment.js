8d4d12fae1c1b2d9b1e2c86421b4cfaa955b5634
github username <github email address>
2021-04-24 10:53:00 -0400

    added popper dependency

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,5 +4,6 @@ const webpack = require("webpack")
 environment.plugins.append("Provide", new webpack.ProvidePlugin({
   $: 'jquery',
   jQuery: 'jquery',
+  Popper: ['popper.js', 'default']
 }))
 module.exports = environment
