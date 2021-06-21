5ae8e49e2689622a6cb7da298a5d78cae3ea527f
John Glennan <glennan20@gmail.com>
2021-06-21 10:51:42 -0400

    added seeds to seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-sport = Sport.create(name: "Soccer")
-league = League.create(sport_id: 1, name: "Premier League")
-event = Event.create(league_id: 1, name: "Brighton vs New Castle")
-bet = Bet.create(event_id: 1, odds: -100)
-betslip = Betslip.new(bet_id: 1, amount: 100)
+# sport = Sport.create(name: "Soccer")
+# league = League.create(sport_id: 1, name: "Premier League")
+# event = Event.create(league_id: 1, name: "Brighton vs New Castle")
+# bet = Bet.create(event_id: 1, odds: -100)
+# betslip = Betslip.new(bet_id: 1, amount: 100)
+
+100.times do 
+    Profile.create([{ name: Faker::Creature::Dog.name, match: 0.5 > rand() ? true : false, image: "https://placedog.net/640/480/?id=#{rand(1..200)}"}])
+end
 
