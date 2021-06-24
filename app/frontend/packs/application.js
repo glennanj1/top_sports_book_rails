5cdb665311f8feeb2e90916cffc7db289cac8340
John Glennan <glennan20@gmail.com>
2021-06-24 10:10:56 -0400

    fixed navbar drop down problem and added pack tag to application view. fixed column issue. changed source path for bootstrap

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -3,23 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import '../js/bootstrap_js_files.js'
+
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-
-
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-
-//= require popper
-//= require bootstrap-sprockets
-//= require_tree .
-
-// require('jquery')
-//= require jquery_ujs
-//= require turbolinks
