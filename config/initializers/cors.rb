bd55a0ab8bfe5985f007e24ed5f974c52c9f642e
John Glennan <glennan20@gmail.com>
2021-06-28 11:57:21 -0400

    fixed config variable on heroku resetting cors permissions after debug session

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,12 +6,11 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-    allow do
-      origins '*'
-  
-      resource '*',
-        headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head]
-    end
+  allow do
+    origins 'https://tailswipes.netlify.app'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
-  
+end
