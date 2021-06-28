3a1b355e301873f7e4c574d3dd095fb5e6413e54
John Glennan <glennan20@gmail.com>
2021-06-27 21:29:39 -0400

    added view helpers

--- app/helpers/odds_helper.rb
+++ app/helpers/odds_helper.rb
@@ -1,3 +1,10 @@
 module OddsHelper
+    def format_time(time)
+        time += Time.zone_offset('EST')
+        time.strftime('%I:%M%p')
+    end
+    def format_date(date)
+        date.strftime('%m/%d/%Y')
+    end
 
 end
