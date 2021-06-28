7dc8ab380672f06e6a945de3d660e67d76a6e3c0
John Glennan <glennan20@gmail.com>
2021-06-28 08:30:47 -0400

    testing cors origin since places fetch is failing testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'https://tailswipes.netlify.app'
+      origins '*'
   
       resource '*',
         headers: :any,
