ccf0350679e841db1f05bf5547804982157a3c2a
glennanj1 <glennan20@gmail.com>
2021-07-20 17:22:29 -0400

    testing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ENV['CORS'].split(',').map { |o| o.strip }
+    origins '*'
 
     resource '*',
       headers: :any,
